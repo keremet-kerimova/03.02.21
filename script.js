0c30cb244a25496a00b05721fbe947a8b52e99b3
Your Name <you@example.com>
2021-02-02 17:08:35 +0600

    added tab

---
+++ script.js
@@ -0,0 +1,6 @@
+for (a = 1; a <= 10; a++){
+    for(b= 1; b <= 10; b++){
+        document.write("<table><td><th>"+ a + " * " + b + " = " + a * b + "<th><td></table>");
+    }
+    document.write("<br>")
+}
